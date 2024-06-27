f36286ac80931a90ae7f34f6e5ac0fec5d2bfe2b
Ian Hawley <ian@speckle.systems>
2024-06-27 20:22:07 +0100

    added schema value <- before trying to remove core dependency>

---
+++ src/Speckle.Core/SpeckleObjectSchema.cs
@@ -0,0 +1,11 @@
+﻿namespace Speckle.Core;
+
+// POC: core needs to know the version of the schema we have baked into the Objects DLL
+// you'll notice this version isn't IN the objects DLL, that's because Objects is dependent upon core
+// but core needs to know the current schema version we have loaded. Core should depend on objects and NOT
+// the other way around. This needs some consideration but for POC of object versioning, this is where it lives.
+public class SpeckleObjectSchema
+{
+  // POC: I'm not sure about using the Version object ATM, strings may be just as a straight-forward... 
+  public static readonly Version Version = new Version(3, 0, 0);
+}
