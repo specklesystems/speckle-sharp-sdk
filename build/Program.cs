c95e3f04f6180efdffb7fe582b6ba6e414975efd
Adam Hathcock <adam@speckle.systems>
2024-07-04 09:35:50 +0100

    code coverage

--- build/Program.cs
+++ build/Program.cs
@@ -51,7 +51,7 @@ Target(
   Glob.Files(".", "**/*.Tests.Unit.csproj").Concat(Glob.Files(".", "**/*.Tests.csproj")),
   async file =>
   {
-    await RunAsync("dotnet", $"test {file} -c Release --no-build --verbosity=normal");
+    await RunAsync("dotnet", $"test {file} -c Release --no-build --no-restore --verbosity=normal  /p:AltCover=true");
   }
 );
 
