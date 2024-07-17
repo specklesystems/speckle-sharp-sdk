458693fc7b0f57c76c614854254ae1fc3cde9278
oguzhankoral <oguzhankoral@gmail.com>
2024-07-17 15:34:16 +0200

    Add group proxy

--- src/Speckle.Core/Models/Instances/InstanceProxy.cs
+++ src/Speckle.Core/Models/Instances/InstanceProxy.cs
@@ -10,17 +10,17 @@ public class InstanceProxy : Base, IInstanceComponent
   /// <summary>
   /// The definition id as present in the original host app. On receive, it will be mapped to the newly created definition id.
   /// </summary>
-  public string DefinitionId { get; set; }
+  public string definitionId { get; set; }
 
   /// <summary>
   /// The transform of the instance reference.
   /// </summary>
-  public Matrix4x4 Transform { get; set; }
+  public Matrix4x4 transform { get; set; }
 
   /// <summary>
   /// The units of the host application file.
   /// </summary>
-  public string Units { get; set; } = Kits.Units.Meters;
+  public string units { get; set; } = Kits.Units.Meters;
 
-  public int MaxDepth { get; set; }
+  public int maxDepth { get; set; }
 }
