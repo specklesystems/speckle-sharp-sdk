a2f0270a6e128297f6a044cdf61842f5697270f3
Claire Kuang <kuang.claire@gmail.com>
2024-07-17 14:22:27 +0100

    adds old collections namespace deprecation

---
+++ src/Speckle.Core/Models/Deprecated/Collection.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Speckle.Core.Models.Deprecated;
+
+[Obsolete("Replaced by " + nameof(Speckle.Core.Models.Collections.Collection))]
+public class Collection : Speckle.Core.Models.Collections.Collection
+{
+  //Deserializer target DUI3 Collection objects in the `Speckle.Core.Models` namespace
+
+  //Speckle.Core.Models.Deprecated.Collection:Speckle.Core.Models.Collections.Collection
+}
