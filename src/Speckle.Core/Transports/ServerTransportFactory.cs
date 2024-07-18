a7ec855ce9bb3fb988e0de4c5387c56ace730f47
Adam Hathcock <adam@speckle.systems>
2024-07-18 09:45:17 +0100

    exclude factory from tests

--- src/Speckle.Core/Transports/ServerTransportFactory.cs
+++ src/Speckle.Core/Transports/ServerTransportFactory.cs
@@ -1,9 +1,11 @@
+using System.Diagnostics.CodeAnalysis;
 using Speckle.Core.Credentials;
 using Speckle.InterfaceGenerator;
 
 namespace Speckle.Core.Transports;
 
 [GenerateAutoInterface]
+[ExcludeFromCodeCoverage] //factories don't need coverage
 public class ServerTransportFactory : IServerTransportFactory
 {
   public IServerTransport Create(Account account, string streamId, int timeoutSeconds, string? blobStorageFolder) =>
