957a284332e373d3a11551bebf2e365f5bcdfb07
Adam Hathcock <adamhathcock@users.noreply.github.com>
2024-07-23 12:35:27 +0100

    Change DI sig for Server and adjust helpers (#46)
    * Change DI sig for Server and adjust helpers
    
    * Remove V1 server transport

--- src/Speckle.Core/Transports/ServerTransportFactory.cs
+++ src/Speckle.Core/Transports/ServerTransportFactory.cs
@@ -8,6 +8,10 @@ namespace Speckle.Core.Transports;
 [ExcludeFromCodeCoverage] //factories don't need coverage
 public class ServerTransportFactory : IServerTransportFactory
 {
-  public IServerTransport Create(Account account, string streamId, int timeoutSeconds, string? blobStorageFolder) =>
-    new ServerTransport(account, streamId, timeoutSeconds, blobStorageFolder);
+  public IServerTransport Create(
+    Account account,
+    string streamId,
+    int timeoutSeconds = 60,
+    string? blobStorageFolder = null
+  ) => new ServerTransport(account, streamId, timeoutSeconds, blobStorageFolder);
 }
