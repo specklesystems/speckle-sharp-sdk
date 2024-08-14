70729d5ddd1fb0771a504db92457f51d04cbcc58
Claire Kuang <kuang.claire@gmail.com>
2024-08-14 17:57:15 +0200

    feat(gis): CNX-285 new gis feature classes (#73)
    * new gis feature classes with interface for attributes
    
    * updates file names to correspond with classes and adds multipatch feature class
    
    ---------
    
    Co-authored-by: Adam Hathcock <adamhathcock@users.noreply.github.com>

---
+++ src/Speckle.Objects/GIS/GisPointFeature.cs
@@ -0,0 +1,21 @@
+using Speckle.Newtonsoft.Json;
+using Speckle.Objects.Geometry;
+using Speckle.Sdk.Models;
+
+namespace Speckle.Objects.GIS;
+
+[SpeckleType("Objects.GIS.GisPointFeature")]
+public class GisPointFeature : Base, IGisFeature, IDisplayValue<List<Point>>
+{
+  public required Base attributes { get; set; }
+
+  [JsonIgnore]
+  public required List<Point> geometry
+  {
+    get { return displayValue; }
+    set { displayValue = value; }
+  }
+
+  [DetachProperty]
+  public List<Point> displayValue { get; set; }
+}
