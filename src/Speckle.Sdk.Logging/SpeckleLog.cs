8ecf60ad63837fc373b5e563b7ad3ac48bcd4a7e
Adam Hathcock <adam@speckle.systems>
2024-09-02 13:00:53 +0100

    make logger be internal only

--- src/Speckle.Sdk.Logging/SpeckleLog.cs
+++ src/Speckle.Sdk.Logging/SpeckleLog.cs
@@ -4,7 +4,7 @@ namespace Speckle.Sdk.Logging;
 
 public static class SpeckleLog
 {
-  public static Serilog.ILogger SpeckleLogger { get; set; } = Serilog.Core.Logger.None;
+  internal static Serilog.ILogger SpeckleLogger { get; set; } = Serilog.Core.Logger.None;
   public static ISpeckleLogger Logger => new SpeckleLogger(SpeckleLogger);
 
   public static ISpeckleLogger Create(string name) =>
