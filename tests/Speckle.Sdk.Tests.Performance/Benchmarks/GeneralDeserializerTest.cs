c4ebe927c0a4892df2348e72b54f8ec81b6b35f1
Adam Hathcock <adam@speckle.systems>
2024-09-05 08:37:23 +0100

    can complete tests

--- tests/Speckle.Sdk.Tests.Performance/Benchmarks/GeneralDeserializerTest.cs
+++ tests/Speckle.Sdk.Tests.Performance/Benchmarks/GeneralDeserializerTest.cs
@@ -10,11 +10,6 @@ using Speckle.Sdk.Serialisation;
 
 namespace Speckle.Sdk.Tests.Performance.Benchmarks;
 
-public static class JobExtensions
-{
-  public static Job WithConstants(this Job job, string constants) => job.WithArguments([new MsBuildArgument("/p:DefineConstants=\"" + constants + "\"")
-  ]);
-}
 /// <summary>
 /// How many threads on our Deserializer is optimal
 /// </summary>
@@ -31,39 +26,29 @@ public class GeneralDeserializer : IDisposable
       var baseJob = Job.ShortRun.WithLaunchCount(1).WithWarmupCount(0).WithIterationCount(1)
         .WithToolchain(CsProjCoreToolchain.NetCoreApp80);
 
-      var newJob = baseJob.WithNuGet("Speckle.Sdk", "3.1.0-dev.121").WithId("3.1.0-dev.121");
-      var oldJob = baseJob.WithNuGet("Speckle.Sdk", "3.1.0-dev.124").WithId("3.1.0-dev.124");
+      var newJob = baseJob.WithSpeckle("3.1.0-dev.121");
+      var oldJob = baseJob.WithSpeckle("3.1.0-dev.124");
       
       //AddJob(baseJob.WithNuGet("Speckle.Sdk", "3.1.0-dev.122").WithId("3.1.0-dev.122"));
       AddJob(newJob);
       AddJob(oldJob);
     }
   }
-  private TestDataHelper _dataSource;
 
   [GlobalSetup]
-  public async Task Setup()
+  public void Setup()
   {
     TypeLoader.Initialize(typeof(Base).Assembly, typeof(Point).Assembly);
-    _dataSource = new TestDataHelper();
-    await _dataSource
-      .SeedTransport(new("https://latest.speckle.systems/projects/2099ac4b5f/models/da511c4d1e"))
-      .ConfigureAwait(false);
   }
   [Benchmark]
   public async Task<Base> RunTest()
   {
-    SpeckleObjectDeserializer sut = new() { ReadTransport = _dataSource.Transport };
-    return await sut.Deserialize(_dataSource.Transport.GetObject(_dataSource.ObjectId)!);
-  }
-  [GlobalCleanup]
-  public void Cleanup()
-  {
-    Dispose();
-  }
-
-  public void Dispose()
-  {
-    _dataSource.Dispose();
+    using var dataSource = new TestDataHelper();
+    await dataSource
+      .SeedTransport(new("https://latest.speckle.systems/projects/2099ac4b5f/models/da511c4d1e"))
+      .ConfigureAwait(false);
+    
+    SpeckleObjectDeserializer sut = new() { ReadTransport = dataSource.Transport };
+    return await sut.Deserialize(dataSource.Transport.GetObject(dataSource.ObjectId)!);
   }
 }
