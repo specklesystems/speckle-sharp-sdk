57f0f240f5fb7b97195f3bc60f0ca230bbc66873
Jedd Morgan <45512892+JR-Morgan@users.noreply.github.com>
2024-09-10 13:24:03 +0100

    removed comments

--- src/Speckle.Objects/Primitive/Interval2d.cs
+++ src/Speckle.Objects/Primitive/Interval2d.cs
@@ -6,10 +6,8 @@ namespace Speckle.Objects.Primitive;
 [SpeckleType("Objects.Primitive.Interval2d")]
 public class Interval2d : Base
 {
-  //Main empty constructor, `required` keyword enforces setting u and v in initialiser
   public Interval2d() { }
 
-  //Alternate heavy constructor, `SetRequiredMembers` used to disuade users from also setting u and v in initialiser
   [SetsRequiredMembers]
   public Interval2d(double start_u, double end_u, double start_v, double end_v)
   {
