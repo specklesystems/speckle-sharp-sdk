df5b151c90b36f3086cbafc7e7bfd4bfb397ef45
Jedd Morgan <45512892+JR-Morgan@users.noreply.github.com>
2024-09-16 08:41:26 +0100

    Test Coverage (#117)

--- tests/Speckle.Sdk.Tests.Unit/Common/UnitsTest.cs
+++ tests/Speckle.Sdk.Tests.Unit/Common/UnitsTest.cs
@@ -8,11 +8,15 @@ public class UnitsTest
 {
   private const double EPS = 0.00022;
 
+  public static List<string> OfficiallySupportedUnits => Units.SupportedUnits;
+  public static List<string> NotSupportedUnits => ["feeters", "liters", "us_ft"];
+  public static List<string?> ConversionSupport => [.. Units.SupportedUnits, null];
+
   [Test, Combinatorial]
   [DefaultFloatingPointTolerance(EPS)]
   public void TestUnitConversion(
-    [ValueSource(typeof(Units), nameof(Units.SupportedUnits))] string from,
-    [ValueSource(typeof(Units), nameof(Units.SupportedUnits))] string to
+    [ValueSource(nameof(ConversionSupport))] string? from,
+    [ValueSource(nameof(ConversionSupport))] string? to
   )
   {
     var forwards = Units.GetConversionFactor(from, to);
@@ -23,5 +27,44 @@ public class UnitsTest
       Is.EqualTo(1d),
       $"Behaviour says that 1{from} == {forwards}{to}, and 1{to} == {backwards}{from}"
     );
+  }
+
+  [TestCaseSource(nameof(OfficiallySupportedUnits))]
+  public void IsUnitSupported_ReturnsTrue_AllSupportedUnits(string unit)
+  {
+    bool res = Units.IsUnitSupported(unit);
+    Assert.That(res, Is.True);
+  }
+
+  [TestCaseSource(nameof(NotSupportedUnits))]
+  public void IsUnitSupported_ReturnsFalse_NotSupportedUnits(string unit)
+  {
+    bool res = Units.IsUnitSupported(unit);
+    Assert.That(res, Is.False);
+  }
+
+  [TestCaseSource(nameof(OfficiallySupportedUnits))]
+  public void GetUnitsFromString_ReturnsSupported(string unit)
+  {
+    var lower = Units.GetUnitsFromString(unit);
+    var upper = Units.GetUnitsFromString(unit?.ToUpperInvariant());
+
+    Assert.That(lower, Is.EqualTo(unit));
+    Assert.That(upper, Is.EqualTo(unit));
+  }
+
+  [TestCaseSource(nameof(NotSupportedUnits))]
+  public void GetUnitsFromString_ThrowsUnSupported(string unit)
+  {
+    Assert.Throws<ArgumentOutOfRangeException>(() => _ = Units.GetUnitsFromString(unit));
+  }
+
+  [TestCaseSource(nameof(OfficiallySupportedUnits))]
+  public void UnitEncoding_RoundTrip(string unit)
+  {
+    var encoded = Units.GetEncodingFromUnit(unit);
+    var res = Units.GetUnitFromEncoding(encoded);
+
+    Assert.That(res, Is.EqualTo(unit));
   }
 }
