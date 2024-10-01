30b44016dff45795c191be4933ee3f0afe15a195
Adam Hathcock <adam@hathcock.uk>
2024-10-01 17:24:02 +0100

    async stream for progress

--- src/Speckle.Sdk/Transports/ServerUtils/ProgressStream.cs
+++ src/Speckle.Sdk/Transports/ServerUtils/ProgressStream.cs
@@ -12,20 +12,67 @@ internal sealed class ProgressStream(Stream input, long? streamLength, Func<Prog
 
   public override void SetLength(long value) => throw new NotImplementedException();
 
-  public override int Read(byte[] buffer, int offset, int count)
+  #if NETSTANDARD2_0
+  public override async Task<int> ReadAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
   {
-    int n = _stream.Read(buffer, offset, count);
+    int n = await _stream.ReadAsync(buffer, offset, count, cancellationToken).ConfigureAwait(false);
     _position += n;
-    progress?.Invoke(new(ProgressEvent.DownloadBytes, _position, streamLength));
+    if (progress != null)
+    {
+      await progress.Invoke(new(ProgressEvent.DownloadBytes, _position, streamLength)).ConfigureAwait(false);
+    }
+    return n;
+  }
+  #else
+  public override async ValueTask<int> ReadAsync(Memory<byte> buffer,
+    CancellationToken cancellationToken = default)
+  {
+    int n = await _stream.ReadAsync(buffer, cancellationToken).ConfigureAwait(false);
+ 
     return n;
   }
 
-  public override void Write(byte[] buffer, int offset, int count)
+  public override async Task<int> ReadAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
   {
-    _stream.Write(buffer, offset, count);
-    _position += count;
-    progress?.Invoke(new(ProgressEvent.UploadBytes, _position, streamLength));
+    int n = await ReadAsync(new Memory<byte>(buffer, offset, count), cancellationToken).ConfigureAwait(false);
+    _position += n;
+    if (progress != null)
+    {
+      await progress.Invoke(new(ProgressEvent.DownloadBytes, _position, streamLength)).ConfigureAwait(false);
+    }
+    return n;
   }
+#endif
+
+  public override int Read(byte[] buffer, int offset, int count) => throw new NotImplementedException();
+
+  #if NETSTANDARD2_0
+  public override async Task WriteAsync(byte[] buffer, int offset, int count,
+    CancellationToken cancellationToken)
+  {
+    await _stream.WriteAsync(buffer, offset, count, cancellationToken).ConfigureAwait(false);
+    _position += buffer.Length;
+    if (progress != null)
+    {
+      await progress.Invoke(new(ProgressEvent.UploadBytes, _position, streamLength)).ConfigureAwait(false);
+    }
+  }
+  #else
+  public override async ValueTask WriteAsync(ReadOnlyMemory<byte> buffer,
+    CancellationToken cancellationToken = default)
+  {
+    await _stream.WriteAsync(buffer, cancellationToken).ConfigureAwait(false);
+    _position += buffer.Length;
+    if (progress != null)
+    {
+      await progress.Invoke(new(ProgressEvent.UploadBytes, _position, streamLength)).ConfigureAwait(false);
+    }
+  }
+
+  public override async Task WriteAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken) => await WriteAsync(new Memory<byte>(buffer, offset, count), cancellationToken).ConfigureAwait(false);
+
+#endif
+  public override void Write(byte[] buffer, int offset, int count)=> throw new NotImplementedException();
 
   public override bool CanRead => true;
   public override bool CanSeek => false;
