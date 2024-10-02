0f96820928a1ed8a33c2dfe8edcba7139666b664
oguzhankoral <oguzhankoral@gmail.com>
2024-10-02 20:55:20 +0300

    Get rid of dub code on ExtrusionX SubDX BrepX

--- src/Speckle.Objects/Geometry/BrepX.cs
+++ src/Speckle.Objects/Geometry/BrepX.cs
@@ -3,56 +3,31 @@ using Speckle.Sdk.Models;
 
 namespace Speckle.Objects.Geometry;
 
-public interface IRawEncodedObject {
-  public RawEncoding encodedValue { get; set; } 
+public interface IRawEncodedObject
+{
+  public RawEncoding encodedValue { get; set; }
+}
 
+public abstract class RawEncodedObject : Base, IDisplayValue<List<Mesh>>, IRawEncodedObject, IHasArea, IHasVolume
+{
+  [DetachProperty]
+  public required List<Mesh> displayValue { get; set; }
+
+  [DetachProperty]
+  public required RawEncoding encodedValue { get; set; }
+
+  public required string units { get; set; }
+
+  public double area { get; set; }
+
+  public double volume { get; set; }
 }
 
 [SpeckleType("Objects.Geometry.BrepX")]
-public class BrepX: Base, IDisplayValue<List<Mesh>>, IRawEncodedObject, IHasArea, IHasVolume
-{
-  [DetachProperty]
-  public required List<Mesh> displayValue { get; set; }
-  
-  [DetachProperty]
-  public required RawEncoding encodedValue { get; set; } 
- 
-  public required string units { get; set; }
-  
-  public double area { get; set; }
-  
-  public double volume { get; set; }
-}
+public class BrepX : RawEncodedObject;
 
 [SpeckleType("Objects.Geometry.ExtrusionX")]
-public class ExtrusionX: Base, IDisplayValue<List<Mesh>>, IRawEncodedObject, IHasArea, IHasVolume
-{
-  [DetachProperty]
-  public required List<Mesh> displayValue { get; set; }
-  
-  [DetachProperty]
-  public required RawEncoding encodedValue { get; set; } 
- 
-  public required string units { get; set; }
-  
-  public double area { get; set; }
-  
-  public double volume { get; set; }
-}
+public class ExtrusionX : RawEncodedObject;
 
 [SpeckleType("Objects.Geometry.SubDX")]
-public class SubDX: Base, IDisplayValue<List<Mesh>>, IRawEncodedObject, IHasArea, IHasVolume
-{
-  [DetachProperty]
-  public required List<Mesh> displayValue { get; set; }
-  
-  [DetachProperty]
-  public required RawEncoding encodedValue { get; set; } 
- 
-  public required string units { get; set; }
-  
-  public double area { get; set; }
-  
-  public double volume { get; set; }
-}
-
+public class SubDX : RawEncodedObject;
