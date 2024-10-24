25d5185184e8a605178fefb71e10339b41ab8ddb
KatKatKateryna <kateryna@speckle.systems>
2024-10-24 05:28:04 +0100

    complete missing types

--- src/Speckle.Objects/Other/SymbolLayer.cs
+++ src/Speckle.Objects/Other/SymbolLayer.cs
@@ -12,11 +12,12 @@ public class Symbol : Base
 public class MarkerSymbolLayer : Base, ISymbolLayer
 {
   public string symbolUnits { get; set; }
-  public LineStyleType lineType { get; set; }
+  public LineType lineType { get; set; }
   public double lineWidth { get; set; }
   public int lineColor { get; set; }
   public int displayPriority { get; set; }
 
+  public double angle { get; set; }
   public int fillColor { get; set; }
   public MarkerType type { get; set; }
 }
@@ -25,7 +26,7 @@ public class MarkerSymbolLayer : Base, ISymbolLayer
 public class LineSymbolLayer : Base, ISymbolLayer
 {
   public string symbolUnits { get; set; }
-  public LineStyleType lineType { get; set; }
+  public LineType lineType { get; set; }
   public double lineWidth { get; set; }
   public int lineColor { get; set; }
   public int displayPriority { get; set; }
@@ -35,7 +36,7 @@ public class LineSymbolLayer : Base, ISymbolLayer
 public class FillSymbolLayer : Base, ISymbolLayer
 {
   public string symbolUnits { get; set; }
-  public LineStyleType lineType { get; set; }
+  public LineType lineType { get; set; }
   public double lineWidth { get; set; }
   public int lineColor { get; set; }
   public int displayPriority { get; set; }
