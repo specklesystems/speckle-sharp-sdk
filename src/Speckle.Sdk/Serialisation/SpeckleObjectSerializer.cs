b587e460ce7efdafb06b7c9c62f16de339d380c8
Adam Hathcock <adam@hathcock.uk>
2024-11-06 17:19:24 +0000

    move

--- src/Speckle.Sdk/Serialisation/SpeckleObjectSerializer.cs
+++ src/Speckle.Sdk/Serialisation/SpeckleObjectSerializer.cs
@@ -8,6 +8,7 @@ using Speckle.Newtonsoft.Json;
 using Speckle.Sdk.Common;
 using Speckle.Sdk.Helpers;
 using Speckle.Sdk.Models;
+using Speckle.Sdk.Serialisation.Utilities;
 using Speckle.Sdk.Serialisation.V2.Send;
 using Speckle.Sdk.Transports;
 
