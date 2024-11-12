87ff20ee56f7f985907a7cfc70ed1bfcb839bb6d
Adam Hathcock <adam@hathcock.uk>
2024-11-12 09:42:46 +0000

    use default complete

--- src/Speckle.Sdk.Dependencies/Serialization/ChannelSaver.cs
+++ src/Speckle.Sdk.Dependencies/Serialization/ChannelSaver.cs
@@ -17,7 +17,6 @@ public abstract class ChannelSaver
   private const int MAX_PARALLELISM_HTTP = 4;
   private const int MAX_CACHE_WRITE_PARALLELISM = 1;
   private const int MAX_CACHE_BATCH = 200;
-  private const string DUMMY = "dummy";
 
   private readonly Channel<BaseItem> _checkCacheChannel = Channel.CreateUnbounded<BaseItem>();
 
@@ -28,7 +27,7 @@ public abstract class ChannelSaver
       .WithTimeout(HTTP_BATCH_TIMEOUT)
       .PipeAsync(
         MAX_PARALLELISM_HTTP,
-        async x => await SendToServerInternal(x, cancellationToken).ConfigureAwait(false),
+        async x => await SendToServer(x, cancellationToken).ConfigureAwait(false),
         -1,
         false,
         cancellationToken
@@ -36,47 +35,19 @@ public abstract class ChannelSaver
       .Join()
       .Batch(MAX_CACHE_BATCH)
       .WithTimeout(HTTP_BATCH_TIMEOUT)
-      .ReadAllConcurrently(MAX_CACHE_WRITE_PARALLELISM, SaveToCacheInternal, cancellationToken);
+      .ReadAllConcurrently(MAX_CACHE_WRITE_PARALLELISM, SaveToCache, cancellationToken);
     return t;
   }
 
   public async Task Save(BaseItem item, CancellationToken cancellationToken = default) =>
     await _checkCacheChannel.Writer.WriteAsync(item, cancellationToken).ConfigureAwait(false);
 
-  private async Task<List<BaseItem>> SendToServerInternal(
-    List<BaseItem> batch,
-    CancellationToken cancellationToken = default
-  )
-  {
-    var ending = batch.Select(x => x.Id).Contains(DUMMY);
-    if (ending)
-    {
-      batch.RemoveAll(x => x.Id == DUMMY);
-    }
-    var results = await SendToServer(batch, cancellationToken).ConfigureAwait(false);
-    if (ending)
-    {
-      results.Add(new BaseItem(DUMMY, DUMMY, false));
-    }
-    return results;
-  }
-
   public abstract Task<List<BaseItem>> SendToServer(List<BaseItem> batch, CancellationToken cancellationToken);
 
-  public async Task Done() => await Save(new BaseItem(DUMMY, DUMMY, false)).ConfigureAwait(false);
-
-  private void SaveToCacheInternal(List<BaseItem> batch)
+  public Task Done()
   {
-    var ending = batch.Select(x => x.Id).Contains(DUMMY);
-    if (ending)
-    {
-      batch.RemoveAll(x => x.Id == DUMMY);
-    }
-    SaveToCache(batch);
-    if (ending)
-    {
-      _checkCacheChannel.Writer.Complete();
-    }
+    _checkCacheChannel.Writer.Complete();
+    return Task.CompletedTask;
   }
 
   public abstract void SaveToCache(List<BaseItem> item);
