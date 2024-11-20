9a53cfc71d51a16fc74cefcc736fdcf5d568a8cc
Adam Hathcock <adam@hathcock.uk>
2024-11-20 08:16:01 +0000

    format

--- src/Speckle.Sdk/Serialisation/V2/Send/ObjectSerializer.cs
+++ src/Speckle.Sdk/Serialisation/V2/Send/ObjectSerializer.cs
@@ -100,13 +100,12 @@ public class ObjectSerializer : IObjectSerializer
       // Note: this change was needed as we've made the ObjectReference type inherit from Base for
       // the purpose of the send object (connector/conversion level) caching.
       case ObjectReference r:
-        Dictionary<string, object?> ret =
-          new()
-          {
-            ["speckle_type"] = r.speckle_type,
-            ["referencedId"] = r.referencedId,
-            ["__closure"] = r.closure,
-          };
+        Dictionary<string, object?> ret = new()
+        {
+          ["speckle_type"] = r.speckle_type,
+          ["referencedId"] = r.referencedId,
+          ["__closure"] = r.closure,
+        };
         //references can be externally provided and need to know the ids in the closure and reference here
         //AddClosure can take the same value twice
         foreach (var kvp in r.closure.Empty())
