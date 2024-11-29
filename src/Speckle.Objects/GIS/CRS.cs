fd7874e92f2ae59da62aacab0107f9a09b3be775
KatKatKateryna <kateryna@speckle.systems>
2024-11-29 12:33:13 +0000

    required keywords for CRS

--- src/Speckle.Objects/GIS/CRS.cs
+++ src/Speckle.Objects/GIS/CRS.cs
@@ -5,9 +5,9 @@ namespace Speckle.Objects.GIS;
 [SpeckleType("Objects.GIS.CRS")]
 public class CRS : Base
 {
-  public string? name { get; set; }
-  public string? authority_id { get; set; }
-  public string? wkt { get; set; }
+  public required string name { get; set; }
+  public required string? authority_id { get; set; }
+  public required string wkt { get; set; }
   public string? units_native { get; set; }
   public float? offset_x { get; set; }
   public float? offset_y { get; set; }
