b189f466c5cba0fd2e01f662015c0bcf86194a77
Adam Hathcock <adam@hathcock.uk>
2024-12-04 12:25:46 +0000

    add explicit interface tests

---
+++ tests/Speckle.Sdk.Serialization.Tests/ExplicitInterfaceTests.cs
@@ -0,0 +1,63 @@
+﻿using NUnit.Framework;
+using Shouldly;
+using Speckle.Sdk.Host;
+using Speckle.Sdk.Models;
+using Speckle.Sdk.Serialisation.V2.Send;
+
+namespace Speckle.Sdk.Serialization.Tests;
+
+public class ExplicitInterfaceTests
+{
+  [SetUp]
+  public void Setup()
+  {
+    TypeLoader.Reset();
+    TypeLoader.Initialize(typeof(Base).Assembly, typeof(TestClass).Assembly);
+  }
+
+  [Test]
+  public async Task Test_Json()
+  {
+    var testClass = new TestClass() { RegularProperty = "Hello" };
+
+    var objects = new Dictionary<string, string>();
+    var process2 = new SerializeProcess(
+      null,
+      new DummySendCacheManager(objects),
+      new DummyServerObjectManager(),
+      new BaseChildFinder(new BasePropertyGatherer()),
+      new ObjectSerializerFactory(new BasePropertyGatherer()),
+      new SerializeProcessOptions(false, false, true)
+    );
+    await process2.Serialize(testClass, default).ConfigureAwait(false);
+    objects.Count.ShouldBe(1);
+    objects["daaa67cfd73a957247cf2d631b7ca4f3"]
+      .ShouldBe(
+        "{\"RegularProperty\":\"Hello\",\"applicationId\":null,\"speckle_type\":\"Speckle.Core.Serialisation.TestClass\",\"id\":\"daaa67cfd73a957247cf2d631b7ca4f3\"}"
+      );
+  }
+
+  [Test]
+  public void Test_ExtractAllProperties()
+  {
+    var testClass = new TestClass() { RegularProperty = "Hello" };
+
+    var gatherer = new BasePropertyGatherer();
+    var properties = gatherer.ExtractAllProperties(testClass).ToList();
+    properties.Count.ShouldBe(3);
+    properties.Select(x => x.Name).ShouldContain("RegularProperty");
+    properties.Select(x => x.Name).ShouldNotContain("TestProperty");
+  }
+}
+
+[SpeckleType("Speckle.Core.Serialisation.TestClass")]
+public sealed class TestClass : Base, ITestInterface
+{
+  public string RegularProperty { get; set; }
+  string ITestInterface.TestProperty => RegularProperty;
+}
+
+public interface ITestInterface
+{
+  string TestProperty { get; }
+}
