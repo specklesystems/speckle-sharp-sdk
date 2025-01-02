a96db42f16156abf7311c26a9307c4f60f0ccd08
Adam Hathcock <adam@hathcock.uk>
2025-01-02 10:32:29 +0000

    revert change

--- src/Speckle.Sdk.Dependencies/Serialization/ChannelSaver.cs
+++ src/Speckle.Sdk.Dependencies/Serialization/ChannelSaver.cs
@@ -12,7 +12,7 @@ public abstract class ChannelSaver<T>
   private static readonly TimeSpan HTTP_BATCH_TIMEOUT = TimeSpan.FromSeconds(2);
   private const int MAX_PARALLELISM_HTTP = 4;
   private const int HTTP_CAPACITY = 50;
-  private const int MAX_CACHE_WRITE_PARALLELISM = 4;
+  private const int MAX_CACHE_WRITE_PARALLELISM = 1;
   private const int MAX_CACHE_BATCH = 200;
 
   private bool _enabled;
