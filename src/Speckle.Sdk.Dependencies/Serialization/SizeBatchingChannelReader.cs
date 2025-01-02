c84e9ccef73166ff3bd08843f5f86008adfb2fc0
Adam Hathcock <adam@hathcock.uk>
2025-01-02 09:24:23 +0000

    move batch it's own file

--- src/Speckle.Sdk.Dependencies/Serialization/SizeBatchingChannelReader.cs
+++ src/Speckle.Sdk.Dependencies/Serialization/SizeBatchingChannelReader.cs
@@ -8,30 +8,6 @@ public interface IHasSize
   int Size { get; }
 }
 
-public class Batch<T>(int capacity) : IHasSize
-  where T : IHasSize
-{
-#pragma warning disable IDE0032
-  private readonly List<T> _items = new(capacity);
-  private int _batchSize;
-#pragma warning restore IDE0032
-
-  public void Add(T item)
-  {
-    _items.Add(item);
-    _batchSize += item.Size;
-  }
-
-  public void TrimExcess()
-  {
-    _items.TrimExcess();
-    _batchSize = _items.Sum(x => x.Size);
-  }
-
-  public int Size => _batchSize;
-  public List<T> Items => _items;
-}
-
 public class SizeBatchingChannelReader<T>(
   ChannelReader<T> source,
   int batchSize,
