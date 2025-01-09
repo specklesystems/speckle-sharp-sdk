465f635142abe2ef9c2db791570f659a159c9ed3
Adam Hathcock <adamhathcock@users.noreply.github.com>
2025-01-09 11:04:14 +0000

    Add details to sqlite exceptions  (#198)
    * add ServerObjectManagerFactory
    
    * add usage of a command pool
    
    * add more disposal
    
    * save saving increase
    
    * fix tests
    
    * fixes
    
    * push out concurrency and disposablity
    
    * Add a custom task scheduler
    
    * Better usage, don't wait to enqueue to save to channels
    
    * Completely pre-cal batch size to avoid spinning issues
    
    * Try to fix cache counting
    
    * properly dispose things
    
    * format
    
    * clean up
    
    * adjust count and save on current thread
    
    * move batch it's own file
    
    * update a few packages
    
    * fix build and add batch tests
    
    * revert and format
    
    * Revert "save saving increase"
    
    This reverts commit 3b50c857fb9a9da3fbf8193d36b149d2e0d2cb72.
    
    * revert change
    
    * adjust and add tests
    
    * Dispose sqlite manager properly
    
    * Make Batch a IMemoryOwner to allow for pooling
    
    * Fix tests
    
    * Upgrade some deps
    
    * try to make tests more explicit
    
    * remove return value
    
    * Add detailed SqLiteJsonCacheException
    
    * details changes

---
+++ tests/Speckle.Sdk.Tests.Unit/SQLite/SQLiteJsonExceptionTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Data.Sqlite;
+using NUnit.Framework;
+using Speckle.Sdk.SQLite;
+
+namespace Speckle.Sdk.Tests.Unit.SQLite;
+
+[TestFixture]
+public class SqLiteJsonCacheExceptionTests
+{
+  [Test]
+  public void ExpectedExceptionFires_Void()
+  {
+    using var pool = new CacheDbCommandPool("DataSource=:memory:", 1);
+    Assert.Throws<SqLiteJsonCacheException>(
+      () => pool.Use(CacheOperation.Get, new Action<SqliteCommand>(_ => throw new SqliteException("test", 1, 1)))
+    );
+  }
+
+  [Test]
+  public void ExpectedExceptionFires_Return()
+  {
+    using var pool = new CacheDbCommandPool("DataSource=:memory:", 1);
+    Assert.Throws<SqLiteJsonCacheException>(
+      () => pool.Use(CacheOperation.Get, new Func<SqliteCommand, bool>(_ => throw new SqliteException("test", 1, 1)))
+    );
+  }
+}
