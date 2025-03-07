793bbb9cd3152bdc4780a160eb78187810f1ec36
Adam Hathcock <adam@hathcock.uk>
2025-03-07 09:29:05 +0000

    Complete speckle http

--- tests/Speckle.Sdk.Tests.Unit/Helpers/SpeckleHttpTests.cs
+++ tests/Speckle.Sdk.Tests.Unit/Helpers/SpeckleHttpTests.cs
@@ -1,4 +1,5 @@
 ﻿using System.Net;
+using FluentAssertions;
 using Microsoft.Extensions.Logging;
 using Moq;
 using RichardSzalay.MockHttp;
@@ -21,7 +22,7 @@ public class SpeckleHttpTests : MoqTest
     var uri = new Uri("https://speckle.xyz");
     mockHttp.When(uri.AbsoluteUri).Respond("application/json", "{}");
     var response = await speckleHttp.HttpPing(uri, mockHttp.ToHttpClient());
-    Assert.NotNull(response);
+    response.Should().NotBeNull();
   }
 
   [Fact]
@@ -38,5 +39,28 @@ public class SpeckleHttpTests : MoqTest
     await Assert.ThrowsAsync<HttpRequestException>(
       async () => await speckleHttp.HttpPing(uri, mockHttp.ToHttpClient())
     );
+  }
+
+  [Fact]
+  public void CreateHttpClient_NoToken()
+  {
+    var clientHandlerFactory = Create<ISpeckleHttpClientHandlerFactory>();
+
+    using var mockHttp1 = new MockHttpMessageHandler();
+    using var mockHttp2 = new MockHttpMessageHandler();
+    var speckleHandler = Create<DelegatingHandler>();
+
+    clientHandlerFactory
+      .Setup(x => x.Create(mockHttp1, SpeckleHttp.DEFAULT_TIMEOUT_SECONDS))
+      .Returns(speckleHandler.Object);
+
+    var speckleHttp = new SpeckleHttp(
+      Create<ILogger<SpeckleHttp>>(MockBehavior.Loose).Object,
+      clientHandlerFactory.Object
+    );
+
+    var client = speckleHttp.CreateHttpClient(mockHttp1);
+    client.Should().NotBeNull();
+    client.DefaultRequestHeaders.Should().BeEmpty();
   }
 }
