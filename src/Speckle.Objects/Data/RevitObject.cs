7f2d57cdade78734ea038f4749e268ab44506181
Claire Kuang <kuang.claire@gmail.com>
2025-03-17 10:05:41 +0000

    adds level

--- src/Speckle.Objects/Data/RevitObject.cs
+++ src/Speckle.Objects/Data/RevitObject.cs
@@ -13,6 +13,13 @@ public class RevitObject : DataObject, IRevitObject
   public required string category { get; set; }
 
   /// <summary>
+  /// The level constraint of the object.
+  /// For objects constrained by multiple levels, this represents the base constraint.
+  /// For objects with no level constraint, this should be "none".
+  /// </summary>
+  public required string level { get; set; }
+
+  /// <summary>
   /// A Curve or Point object representing the location of a Revit element.
   /// </summary>
   public required Base? location { get; set; }
