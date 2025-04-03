c788435c48816bc8a9a47b816c59d2b55a00cdbe
KatKatKateryna <kateryna@speckle.systems>
2025-04-03 02:10:12 +0100

    commit to test linear integration

---
+++ src/Speckle.Objects/Other/Text.cs
@@ -0,0 +1,9 @@
+using Speckle.Sdk.Models;
+
+namespace Speckle.Objects.Other;
+
+/// <summary>
+/// Text class for representation in the viewer
+/// </summary>
+[SpeckleType("Objects.Other.Text")]
+public class Text { }
