67b5f9c88e6458878750ce129f9adaa93d226551
KatKatKateryna <kateryna@speckle.systems>
2025-04-04 01:16:48 +0100

    corrections

--- src/Speckle.Objects/Other/Text.cs
+++ src/Speckle.Objects/Other/Text.cs
@@ -11,12 +11,28 @@ namespace Speckle.Objects.Other;
 [SpeckleType("Objects.Other.Text")]
 public class Text : Base
 {
+  /// <summary>
+  /// The text itself, without formatting
+  /// </summary>
   public required string value { get; set; }
   public required Point origin { get; set; }
-  public required double? height { get; set; }
+
+  /// <summary>
+  /// Height in linear units or pixels (keep "units" as null)
+  /// </summary>
+  public required double height { get; set; }
+
+  /// <summary>
+  /// Units will be null if the text size is defined in pixels (stays the same size
+  /// independently of zooming the model). Default pixel size it 17px (used for Viewer measurements)
+  /// </summary>
   public required string? units { get; set; }
-  public int justificationH { get; set; }
-  public int justificationV { get; set; }
+  public int alignmentH { get; set; }
+  public int alignmentV { get; set; }
   public int color { get; set; } = Color.Black.ToArgb();
+
+  /// <summary>
+  /// Plane will be null if the text object orientation follows camera view
+  /// </summary>
   public Plane? plane { get; set; }
 }
