a2c28653d7581f2bcc48eeaff8ac61e217231fd1
KatKatKateryna <kateryna@speckle.systems>
2025-04-04 01:00:55 +0100

    draft class

--- src/Speckle.Objects/Other/Text.cs
+++ src/Speckle.Objects/Other/Text.cs
@@ -1,4 +1,7 @@
+using System.Drawing;
+using Speckle.Objects.Geometry;
 using Speckle.Sdk.Models;
+using Point = Speckle.Objects.Geometry.Point;
 
 namespace Speckle.Objects.Other;
 
@@ -6,4 +9,14 @@ namespace Speckle.Objects.Other;
 /// Text class for representation in the viewer
 /// </summary>
 [SpeckleType("Objects.Other.Text")]
-public class Text { }
+public class Text : Base
+{
+  public required string value { get; set; }
+  public required Point origin { get; set; }
+  public required double? height { get; set; }
+  public required string? units { get; set; }
+  public int justificationH { get; set; }
+  public int justificationV { get; set; }
+  public int color { get; set; } = Color.Black.ToArgb();
+  public Plane? plane { get; set; }
+}
