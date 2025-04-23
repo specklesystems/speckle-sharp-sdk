ea6ca8c555e6656cca262c72ac407eb834a01139
Adam Hathcock <adam@hathcock.uk>
2025-04-23 15:57:03 +0100

    add registration tests to SDK and ignore classes that can't be made

---
+++ tests/Speckle.Sdk.Tests.Unit/ServiceRegistrationTests.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Speckle.Sdk.Tests.Unit;
+
+public class ServiceRegistrationTests
+{
+  [Fact]
+  public void RegisterDependencies_Validation()
+  {
+    var serviceCollection = new ServiceCollection();
+    serviceCollection.AddSpeckleSdk(new("Tests", "test"), "v3");
+    var serviceProvider = serviceCollection.BuildServiceProvider(new ServiceProviderOptions { ValidateOnBuild = true });
+    serviceProvider.Should().NotBeNull();
+  }
+
+  public void RegisterDependencies_Scopes()
+  {
+    var serviceCollection = new ServiceCollection();
+    serviceCollection.AddSpeckleSdk(new("Tests", "test"), "v3");
+    var serviceProvider = serviceCollection.BuildServiceProvider(
+      new ServiceProviderOptions { ValidateScopes = true, ValidateOnBuild = true }
+    );
+    serviceProvider.Should().NotBeNull();
+  }
+}
