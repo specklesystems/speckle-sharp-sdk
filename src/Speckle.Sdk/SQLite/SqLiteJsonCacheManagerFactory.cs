3217dfdc36addf216482834e0b988cf4bfcff0bc
Adam Hathcock <adam@hathcock.uk>
2025-05-12 16:17:20 +0100

    Use a sqlite manager per stream that's kept around forever

--- src/Speckle.Sdk/SQLite/SqLiteJsonCacheManagerFactory.cs
+++ src/Speckle.Sdk/SQLite/SqLiteJsonCacheManagerFactory.cs
@@ -1,4 +1,5 @@
-﻿using Speckle.InterfaceGenerator;
+﻿using System.Collections.Concurrent;
+using Speckle.InterfaceGenerator;
 using Speckle.Sdk.Logging;
 using Speckle.Sdk.Serialisation.Utilities;
 
@@ -8,12 +9,21 @@ namespace Speckle.Sdk.SQLite;
 public class SqLiteJsonCacheManagerFactory : ISqLiteJsonCacheManagerFactory
 {
   public const int INITIAL_CONCURRENCY = 4;
+  
+  private readonly ConcurrentDictionary<string, ISqLiteJsonCacheManager> _cachedManagers = new();
 
   private ISqLiteJsonCacheManager Create(string path, int concurrency) => new SqLiteJsonCacheManager(path, concurrency);
 
   public ISqLiteJsonCacheManager CreateForUser(string scope) =>
     Create(Path.Combine(SpecklePathProvider.UserApplicationDataPath(), "Speckle", $"{scope}.db"), 1);
 
-  public ISqLiteJsonCacheManager CreateFromStream(string streamId) =>
-    Create(SqlitePaths.GetDBPath(streamId), INITIAL_CONCURRENCY);
+  public ISqLiteJsonCacheManager CreateFromStream(string streamId)
+  {
+    if (!_cachedManagers.TryGetValue(streamId, out var manager))
+    {
+      manager = Create(SqlitePaths.GetDBPath(streamId), INITIAL_CONCURRENCY);
+      _cachedManagers.TryAdd(streamId, manager);
+    }
+    return manager;
+  }
 }
