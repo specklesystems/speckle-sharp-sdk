eb640f83ec88118f94ebf476c72de9c1b7e050f7
Jedd Morgan <45512892+JR-Morgan@users.noreply.github.com>
2025-05-13 16:12:46 +0100

    extra server resource test

---
+++ tests/Speckle.Sdk.Tests.Integration/Api/GraphQL/Resources/ServerResourceTests.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using Speckle.Sdk.Api;
+using Speckle.Sdk.Api.GraphQL.Inputs;
+using Speckle.Sdk.Api.GraphQL.Models;
+using Speckle.Sdk.Api.GraphQL.Resources;
+
+namespace Speckle.Sdk.Tests.Integration.API.GraphQL.Resources;
+
+public class ServerResourceTests : IAsyncLifetime
+{
+  private IClient _testUser;
+  private ServerResource Sut => _testUser.Server;
+
+  public async Task InitializeAsync()
+  {
+    // Runs instead of [SetUp] in NUnit
+    _testUser = await Fixtures.SeedUserWithClient();
+  }
+
+  public Task DisposeAsync()
+  {
+    // Perform any cleanup, if needed
+    return Task.CompletedTask;
+  }
+
+  [Fact]
+  public async Task ExpectWorkspaceNotEnabled()
+  {
+    bool result = await Sut.IsWorkspaceEnabled();
+    result.Should().Be(true);
+  }
+}
