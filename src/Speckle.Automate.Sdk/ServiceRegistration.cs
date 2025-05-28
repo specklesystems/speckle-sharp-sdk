01e1e73a44d79a03222be9a50ce05aa4c0f7d90a
Jedd Morgan <45512892+JR-Morgan@users.noreply.github.com>
2025-05-28 17:51:07 +0100

    Correct the DI registration

--- src/Speckle.Automate.Sdk/ServiceRegistration.cs
+++ src/Speckle.Automate.Sdk/ServiceRegistration.cs
@@ -1,6 +1,5 @@
 ﻿using System.Reflection;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection.Extensions;
 using Speckle.Objects.Geometry;
 using Speckle.Sdk;
 using Speckle.Sdk.Models;
@@ -9,9 +8,8 @@ namespace Speckle.Automate.Sdk;
 
 public static class ServiceRegistration
 {
-  
   /// <summary>
-  /// Sets-up the serviceCollection with all the services in Speckle.Automate.Sdk and Speckle.Sdk 
+  /// Sets-up the serviceCollection with all the services in Speckle.Automate.Sdk and Speckle.Sdk
   /// </summary>
   /// <param name="serviceCollection"></param>
   /// <returns></returns>
@@ -38,33 +36,8 @@ public static class ServiceRegistration
   )
   {
     serviceCollection.AddSpeckleSdk(speckleSdkOptions);
-
-    AddMatchingInterfacesAsTransient(
-      serviceCollection,
-      typeof(ServiceRegistration).Assembly,
-      [typeof(AutomationContext)]
-    );
-
-    return serviceCollection;
-  }
-
-  public static IServiceCollection AddMatchingInterfacesAsTransient(
-    this IServiceCollection serviceCollection,
-    Assembly assembly,
-    params Type[] classesToIgnore
-  )
-  {
-    foreach (var type in assembly.ExportedTypes.Where(t => t.IsNonAbstractClass()))
-    {
-      if (classesToIgnore.Contains(type))
-      {
-        continue;
-      }
-      foreach (var matchingInterface in type.FindMatchingInterface())
-      {
-        serviceCollection.TryAddTransient(matchingInterface, type);
-      }
-    }
+    serviceCollection.AddTransient<IAutomationContextFactory, AutomationContextFactory>();
+    serviceCollection.AddTransient<IAutomationRunner, AutomationRunner>();
 
     return serviceCollection;
   }
