b6be7a351ff7027987676437f4a2def826aad646
Jedd Morgan <45512892+JR-Morgan@users.noreply.github.com>
2025-05-30 13:05:14 +0100

    feat(automate): Add automate SDK (#313)
    * First pass
    
    * First pass adding service registraiton
    
    * Finished up service registration
    
    * Json exception
    
    * Moved to the right place
    
    * Fixed tests
    
    * Added some missing docs strings
    
    * Reflecting Gergo's specklepy changes
    
    * Correct the DI registration
    
    * Readme
    
    * No warn beta packages
    
    * Format
    
    * renamed misleading variable
    
    * Fixed lock files
    
    * Disable SQLite for automate

---
+++ src/Speckle.Automate.Sdk/AutomateContextFactory.cs
@@ -0,0 +1,63 @@
+﻿using System.Diagnostics;
+using GraphQL;
+using GraphQL.Client.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Speckle.Automate.Sdk.Schema;
+using Speckle.InterfaceGenerator;
+using Speckle.Sdk.Api;
+using Speckle.Sdk.Credentials;
+
+namespace Speckle.Automate.Sdk;
+
+[GenerateAutoInterface(VisibilityModifier = "public")]
+internal sealed class AutomationContextFactory(
+  IClientFactory clientFactory,
+  IAccountManager accountManager,
+  IOperations operations
+) : IAutomationContextFactory
+{
+  /// <inheritdoc cref="Initialize(AutomationRunData, string)"/>
+  public async Task<IAutomationContext> Initialize(string automationRunData, string speckleToken)
+  {
+    var runData = JsonConvert.DeserializeObject<AutomationRunData>(
+      automationRunData,
+      new JsonSerializerSettings { ContractResolver = new CamelCasePropertyNamesContractResolver() }
+    );
+    return await Initialize(runData, speckleToken).ConfigureAwait(false);
+  }
+
+  /// <inheritdoc cref="Initialize(AutomationRunData, Account)"/>
+  /// <exception cref="GraphQLHttpRequestException">Request failed on the HTTP layer (received a non-successful response code)</exception>
+  /// <exception cref="AggregateException"><inheritdoc cref="Speckle.Sdk.Api.GraphQL.GraphQLErrorHandler.EnsureGraphQLSuccess(IGraphQLResponse)"/></exception>
+  public async Task<IAutomationContext> Initialize(AutomationRunData automationRunData, string speckleToken)
+  {
+    Account account = new()
+    {
+      token = speckleToken,
+      serverInfo = await accountManager.GetServerInfo(automationRunData.SpeckleServerUrl).ConfigureAwait(false),
+      userInfo = await accountManager
+        .GetUserInfo(speckleToken, automationRunData.SpeckleServerUrl)
+        .ConfigureAwait(false),
+    };
+    return Initialize(automationRunData, account);
+  }
+
+  /// <summary>
+  /// Creates an <see cref="AutomationContext"/> from the provided data
+  /// </summary>
+  public IAutomationContext Initialize(AutomationRunData automationRunData, Account account)
+  {
+    IClient client = clientFactory.Create(account);
+    Stopwatch initTime = Stopwatch.StartNew();
+
+    return new AutomationContext(operations)
+    {
+      AutomationRunData = automationRunData,
+      SpeckleClient = client,
+      _speckleToken = account.token,
+      _initTime = initTime,
+      AutomationResult = new AutomationResult(),
+    };
+  }
+}
