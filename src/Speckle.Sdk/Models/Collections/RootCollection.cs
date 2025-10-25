f1a64590d7780068e54de50e367e959e5ed0cc4a
Björn <steinhagen.bjoern@gmail.com>
2025-10-25 11:59:39 +0200

    chore(models): adds RootCollection

---
+++ src/Speckle.Sdk/Models/Collections/RootCollection.cs
@@ -0,0 +1,27 @@
+namespace Speckle.Sdk.Models.Collections;
+
+/// <summary>
+/// Root collection that represents the top-level commit object.
+/// Extends Collection to include model-wide properties that apply to the entire model.
+/// </summary>
+[SpeckleType("Speckle.Core.Models.Collections.RootCollection")]
+public class RootCollection : Collection
+{
+  /// <summary>
+  /// Constructor for a root collection.
+  /// </summary>
+  /// <param name="name">The human-readable name of this root collection</param>
+  public RootCollection(string name) : base(name) { }
+
+  /// <summary>
+  /// Model-wide properties that apply to the entire model.
+  /// </summary>
+  /// <remarks>
+  /// These are intended for model-level metadata such as total area, project information, or analysis results.
+  /// </remarks>
+  public Dictionary<string, object?>? properties
+  {
+    get => this["properties"] as Dictionary<string, object?>;
+    set => this["properties"] = value;
+  }
+}
