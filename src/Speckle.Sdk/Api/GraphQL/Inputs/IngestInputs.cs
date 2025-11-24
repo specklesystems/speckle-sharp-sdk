937eb94730357ddd2ac41c05f8955d6ad33a5f3d
Jedd Morgan <45512892+JR-Morgan@users.noreply.github.com>
2025-11-24 18:40:17 +0000

    First pass

---
+++ src/Speckle.Sdk/Api/GraphQL/Inputs/IngestInputs.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Speckle.Sdk.Api.GraphQL.Inputs;
+
+public record IngestCreateInput(
+  string fileName,
+  int? maxIdleTimeoutMinutes,
+  string modelId,
+  string projectId,
+  string sourceApplication,
+  string sourceApplicationVersion,
+  IReadOnlyDictionary<string, object?> sourceFileData);
+
+public record IngestFinishInput(string id, string? message, string objectId, string projectId);
+
+public record IngestErrorInput(string errorReason, string errorStacktrace, string id, string projectId);
+
+public record CancelRequestInput(string id, string projectId);
+
+public record IngestUpdateInput(string id, double? progress, string? progressMessage, string projectId);
