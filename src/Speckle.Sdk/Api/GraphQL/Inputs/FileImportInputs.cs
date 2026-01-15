8071990dd5069d52cc3cb0a176c1c6ef10d2152e
Jedd Morgan <45512892+JR-Morgan@users.noreply.github.com>
2026-01-15 16:29:33 +0000

    deprecate file import api (#431)

--- src/Speckle.Sdk/Api/GraphQL/Inputs/FileImportInputs.cs
+++ src/Speckle.Sdk/Api/GraphQL/Inputs/FileImportInputs.cs
@@ -2,8 +2,10 @@
 
 public record GenerateFileUploadUrlInput(string projectId, string fileName);
 
+[Obsolete(FileImportInputBase.FILE_IMPORT_DEPRECATION_MESSAGE)]
 public record StartFileImportInput(string projectId, string modelId, string fileId, string etag);
 
+[Obsolete(FileImportInputBase.FILE_IMPORT_DEPRECATION_MESSAGE)]
 public record FileImportResult(
   double durationSeconds,
   double downloadDurationSeconds,
@@ -14,14 +16,23 @@ public record FileImportResult(
 
 public abstract class FileImportInputBase
 {
+  internal const string FILE_IMPORT_DEPRECATION_MESSAGE =
+    "Part of the old API surface and will be removed in the future. Use the new ingestion API instead. Field will be deleted on June 1st, 2026";
+
+  [Obsolete(FileImportInputBase.FILE_IMPORT_DEPRECATION_MESSAGE)]
+  protected FileImportInputBase() { }
+
   public required string projectId { get; init; }
   public required string jobId { get; init; }
   public required IReadOnlyCollection<string> warnings { get; init; }
+
+  [Obsolete(FileImportInputBase.FILE_IMPORT_DEPRECATION_MESSAGE)]
   public required FileImportResult result { get; init; }
 }
 
 #pragma warning disable CA1822 //Mark members as static
 
+[Obsolete(FILE_IMPORT_DEPRECATION_MESSAGE)]
 public sealed class FileImportSuccessInput() : FileImportInputBase()
 {
   public const string TYPE_STATUS = "success";
@@ -29,6 +40,7 @@ public sealed class FileImportSuccessInput() : FileImportInputBase()
   public string status => TYPE_STATUS;
 }
 
+[Obsolete(FILE_IMPORT_DEPRECATION_MESSAGE)]
 public sealed class FileImportErrorInput() : FileImportInputBase()
 {
   public const string TYPE_STATUS = "error";
