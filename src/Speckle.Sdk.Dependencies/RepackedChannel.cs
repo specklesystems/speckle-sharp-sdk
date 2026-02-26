0110fbca22404087ffee7deb65d36eee1ffa0667
Jedd Morgan <45512892+JR-Morgan@users.noreply.github.com>
2026-02-26 18:08:32 +0000

    ensure channels is inside il repacked project

---
+++ src/Speckle.Sdk.Dependencies/RepackedChannel.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Channels;
+
+namespace Speckle.Sdk.Dependencies;
+
+public sealed class RepackedChannel<T>
+{
+  private readonly Channel<T> _channel;
+
+  public RepackedChannel(int capacity, bool singleReader, bool singleWriter)
+  {
+    _channel = Channel.CreateBounded<T>(
+      new BoundedChannelOptions(capacity)
+      {
+        FullMode = BoundedChannelFullMode.Wait,
+        SingleReader = singleReader,
+        SingleWriter = singleWriter,
+      }
+    );
+  }
+
+  public void CompleteWriter() => _channel.Writer.Complete();
+
+  public ValueTask WriteAsync(T item, CancellationToken cancellationToken) =>
+    _channel.Writer.WriteAsync(item, cancellationToken);
+
+  public IAsyncEnumerable<T> ReadAllAsync(CancellationToken cancellationToken) =>
+    _channel.Reader.ReadAllAsync(cancellationToken);
+
+  public async Task ReadAllAsync(Func<T, Task> callback, CancellationToken cancellationToken)
+  {
+    await foreach (var item in _channel.Reader.ReadAllAsync(cancellationToken).ConfigureAwait(false))
+    {
+      await callback(item).ConfigureAwait(false);
+    }
+  }
+}
--- src/Speckle.Sdk/Pipelines/Send/DiskStore.cs
+++ src/Speckle.Sdk/Pipelines/Send/DiskStore.cs
@@ -2,6 +2,7 @@ using System.IO.Compression;
 using System.Threading.Channels;
 using Microsoft.Extensions.Logging;
 using Speckle.InterfaceGenerator;
+using Speckle.Sdk.Dependencies;
 using Speckle.Sdk.Helpers;
 
 namespace Speckle.Sdk.Pipelines.Send;
@@ -14,7 +15,7 @@ public sealed class DiskStoreFactory(ILogger<DiskStore> logger) : IDiskStoreFact
 
 public sealed class DiskStore
 {
-  private readonly Channel<UploadItem> _channel;
+  private readonly RepackedChannel<UploadItem> _channel;
   private readonly Task<DisposableFile> _writeToDiskTask;
   private readonly ILogger<DiskStore> _logger;
   private readonly CancellationToken _cancellationToken;
@@ -24,17 +25,15 @@ public sealed class DiskStore
     _logger = logger;
     _cancellationToken = cancellationToken;
 
-    _channel = Channel.CreateBounded<UploadItem>(
-      new BoundedChannelOptions(1000) { FullMode = BoundedChannelFullMode.Wait, SingleReader = true }
-    );
+    _channel = new RepackedChannel<UploadItem>(1000, true, false);
     _writeToDiskTask = Task.Run(WriteFile, cancellationToken);
   }
 
-  public ValueTask PushAsync(UploadItem item) => _channel.Writer.WriteAsync(item, _cancellationToken);
+  public ValueTask PushAsync(UploadItem item) => _channel.WriteAsync(item, _cancellationToken);
 
   public async Task<DisposableFile> CompleteAsync()
   {
-    _channel.Writer.Complete();
+    _channel.CompleteWriter();
     return await _writeToDiskTask.ConfigureAwait(false);
   }
 
@@ -55,7 +54,7 @@ public sealed class DiskStore
       using var gzip = new GZipStream(fileStream, CompressionLevel.Optimal);
       using var writer = new StreamWriter(gzip);
 
-      await foreach (var item in _channel.Reader.ReadAllAsync(_cancellationToken).ConfigureAwait(false))
+      await foreach (var item in _channel.ReadAllAsync(_cancellationToken).ConfigureAwait(false))
       {
         await writer.WriteLineAsync($"{item.Id}\t{item.Json}\t{item.SpeckleType}").ConfigureAwait(false);
       }
