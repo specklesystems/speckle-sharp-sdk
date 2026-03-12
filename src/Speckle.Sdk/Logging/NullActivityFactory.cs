d99aef03847afedec3a97986edda6da3ae7277bd
Jedd Morgan <45512892+JR-Morgan@users.noreply.github.com>
2026-03-12 12:36:14 +0000

    Add proper support for remote spans

--- src/Speckle.Sdk/Logging/NullActivityFactory.cs
+++ src/Speckle.Sdk/Logging/NullActivityFactory.cs
@@ -4,5 +4,8 @@ public sealed class NullActivityFactory : ISdkActivityFactory
 {
   public void Dispose() { }
 
-  public ISdkActivity? Start(string? name = default, string source = "", string? parentId = null) => null;
+  public ISdkActivity? Start(string? name = default, string source = "") => null;
+
+  public ISdkActivity? StartRemote(string traceId, string parentSpanId, string? name = default, string source = "") =>
+    null;
 }
