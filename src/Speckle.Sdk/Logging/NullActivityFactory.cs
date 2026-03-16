aff1e3beecbbdcb72a96aab1cddfc4afb463ac07
Jedd Morgan <45512892+JR-Morgan@users.noreply.github.com>
2026-03-16 14:55:58 +0000

    Remote changes

--- src/Speckle.Sdk/Logging/NullActivityFactory.cs
+++ src/Speckle.Sdk/Logging/NullActivityFactory.cs
@@ -1,11 +1,12 @@
-﻿namespace Speckle.Sdk.Logging;
+﻿using Speckle.Connectors.Logging;
+
+namespace Speckle.Sdk.Logging;
 
 public sealed class NullActivityFactory : ISdkActivityFactory
 {
   public void Dispose() { }
 
-  public ISdkActivity? Start(string? name = default, string source = "") => null;
+  public ISdkActivity? Start(string? name, SdkActivityKind kind, string source) => null;
 
-  public ISdkActivity? StartRemote(string traceId, string parentSpanId, string? name = default, string source = "") =>
-    null;
+  public ISdkActivity? StartRemote(string traceContext, SdkActivityKind kind, string? name, string source) => null;
 }
