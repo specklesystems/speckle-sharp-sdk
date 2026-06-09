306628d4a23bcf53df455a40ed9e2b33e20a8170
Jedd Morgan <45512892+JR-Morgan@users.noreply.github.com>
2026-06-09 11:02:38 +0100

    Expose OTEL headers in logging

--- src/Speckle.Sdk.Dependencies/ISdkActivity.cs
+++ src/Speckle.Sdk.Dependencies/ISdkActivity.cs
@@ -3,7 +3,14 @@ namespace Speckle.Sdk.Logging;
 public interface ISdkActivity : IDisposable
 {
   void SetTag(string key, object? value);
-  void RecordException(Exception e);
+  void RecordException(Exception ex);
+
+  // W3C <c>tracestate</c> header
+  string? TraceState { get; }
+
+  // W3C <c>traceparent</c> header
+  string TraceParent { get; }
+
   string TraceId { get; }
   string SpanId { get; }
   void SetStatus(SdkActivityStatusCode code);
