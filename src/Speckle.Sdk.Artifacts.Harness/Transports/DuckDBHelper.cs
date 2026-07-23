4dea642d9452c12734b1996d4c79c66466b7e7fc
Jedd Morgan <45512892+JR-Morgan@users.noreply.github.com>
2026-07-23 11:24:13 +0100

    duckdb helper

---
+++ src/Speckle.Sdk.Artifacts.Harness/Transports/DuckDBHelper.cs
@@ -0,0 +1,41 @@
+﻿using Speckle.Sdk.Pipelines.Progress;
+
+namespace Speckle.Sdk.Artifacts.Harness.Transports;
+
+internal static class DuckDbHelper
+{
+  public static async Task DownloadDuckFile(
+    HttpClient client,
+    string projectId,
+    string modelId,
+    string versionId,
+    FileInfo destination,
+    IProgress<StreamProgressArgs> downloadProgress,
+    CancellationToken cancellationToken
+  )
+  {
+    Uri url = new($"/api/v1/projects/{projectId}/models/{modelId}/versions/{versionId}/download", UriKind.Relative);
+
+    using var response = await client
+      .GetAsync(url, HttpCompletionOption.ResponseHeadersRead, cancellationToken)
+      .ConfigureAwait(false);
+    response.EnsureSuccessStatusCode();
+
+    using var destinationStream = new FileStream(
+      destination.FullName,
+      FileMode.Create,
+      FileAccess.Write,
+      FileShare.None,
+      1024 * 1024,
+      FileOptions.Asynchronous
+    );
+    using ProgressStream progressStream = new(destinationStream, downloadProgress);
+
+#if NET5_0_OR_GREATER
+    await response.Content.CopyToAsync(destinationStream, null, cancellationToken).ConfigureAwait(false);
+#else
+    await response.Content.CopyToAsync(destinationStream).ConfigureAwait(false);
+#endif
+    destination.Refresh();
+  }
+}
